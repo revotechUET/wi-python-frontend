02d12f820b2181214101e29f76568e949af311e6
BuiHoang <bdhoang.ht@gmail.com>
2019-04-16 15:19:27 +0700

    add jwt token to all requests

--- src/_project-api/index.js
+++ src/_project-api/index.js
@@ -1,109 +1,124 @@
-export const name = 'projectApi'
+export const name = 'projectApi';
 
-service.$inject = ['config', 'request']
+service.$inject = ['config', 'request'];
+
 export function service(config, request) {
 
-  const newProject = name => {
-    const user = getUsername()
-    const url = `${config.ONLINE_EDITOR_URL}/project/new?name=${encodeURIComponent(name)}&user=${user}`
-    return request.get(url)
-  }
+	const newProject = name => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/new?name=${encodeURIComponent(name)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const openProject = name => {
-    const url = `${config.ONLINE_EDITOR_URL}/project/open?name=${encodeURIComponent(name)}`
-    return request.get(url)
-  }
+	const openProject = name => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/open?name=${encodeURIComponent(name)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const deleteProject = name => {
-    const url = `${config.ONLINE_EDITOR_URL}/project/delete?name=${encodeURIComponent(name)}`
-    return request.get(url)
-  }
+	const deleteProject = name => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/delete?name=${encodeURIComponent(name)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const openFile = dir => {
-    const url = `${config.ONLINE_EDITOR_URL}/project/read-file?dir=${encodeURIComponent(dir)}`
-    return request.get(url)
-  }
+	const openFile = dir => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/read-file?dir=${encodeURIComponent(dir)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const openFolder = dir => {
-    const url = `${config.ONLINE_EDITOR_URL}/project/read-folder?dir=${encodeURIComponent(dir)}`
-    return request.get(url)
-  }
+	const openFolder = dir => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/read-folder?dir=${encodeURIComponent(dir)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const listProjects = () => {
-    const user = getUsername()
-    const url = `${config.ONLINE_EDITOR_URL}/project/list?user=${user}`
-    return request.get(url)
-  }
+	const listProjects = () => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/project/list?&token=${token}`;
+		return request.get(url)
+	};
 
-  const runCode = (fileName, project) => {
-    const url = `${config.ONLINE_EDITOR_URL}/code-runner/?file=${encodeURIComponent(fileName)}&project=${encodeURIComponent(project)}`
-    return request.get(url)
-  }
+	const runCode = (fileName, project) => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/code-runner/?file=${encodeURIComponent(fileName)}&project=${encodeURIComponent(project)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const saveCode = (project, fileName, code) => {
-    const url = `${config.ONLINE_EDITOR_URL}/code-action/save`
-    const data = {
-      project,
-      fileName,
-      code
-    }
+	const saveCode = (project, fileName, code) => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/code-action/save`;
+		const data = {
+			project,
+			fileName,
+			code,
+			token
+		};
 
-    return request.post(url, data)
-  }
+		return request.post(url, data)
+	};
 
-  const login = (username, password) => {
-    //const url = `${config.USER_RELATED_ROOT_URL}/user/login`
-    const url = `${config.USER_RELATED_ROOT_URL}/login`
-    const data = {
-      username,
-      password
-    }
+	const login = (username, password) => {
+		//const url = `${config.USER_RELATED_ROOT_URL}/user/login`
+		const url = `${config.USER_RELATED_ROOT_URL}/login`;
+		const data = {
+			username,
+			password
+		};
 
-    return request.post(url, data)
-  }
+		return request.post(url, data)
+	};
 
-  // const removeFile = (project, file) => {
-  //   const url = `${config.ONLINE_EDITOR_URL}/file-sys/remove-file?project=${project}&file=${encodeURIComponent(file)}`
-  //   return request.get(url)
-  // }
+	// const removeFile = (project, file) => {
+	//   const url = `${config.ONLINE_EDITOR_URL}/file-sys/remove-file?project=${project}&file=${encodeURIComponent(file)}`
+	//   return request.get(url)
+	// }
 
-  const removeItem = (project, item) => {
-    const url = `${config.ONLINE_EDITOR_URL}/file-sys/remove-folder?project=${project}&folder=${encodeURIComponent(item)}`
-    return request.get(url)
-  }
+	const removeItem = (project, item) => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/file-sys/remove-folder?project=${project}&folder=${encodeURIComponent(item)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const newFile = (project, file) => {
-    const url = `${config.ONLINE_EDITOR_URL}/file-sys/new-file?project=${project}&file=${encodeURIComponent(file)}`
-    return request.get(url)
-  }
+	const newFile = (project, file) => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/file-sys/new-file?project=${project}&file=${encodeURIComponent(file)}&token=${token}`;
+		return request.get(url)
+	};
 
-  const newFolder = (project, folder) => {
-    const url = `${config.ONLINE_EDITOR_URL}/file-sys/new-folder?project=${project}&folder=${encodeURIComponent(folder)}`
-    return request.get(url)
-  }
+	const newFolder = (project, folder) => {
+		const token = getToken();
+		const url = `${config.ONLINE_EDITOR_URL}/file-sys/new-folder?project=${project}&folder=${encodeURIComponent(folder)}&token=${token}`;
+		return request.get(url)
+	};
 
-  function getUsername() {
-    const token = window.localStorage.getItem('JWT_TOKEN')
-    if (!token) throw new Error('token is emtpy')
+	function getToken() {
+		return window.localStorage.getItem('JWT_TOKEN');
+	}
 
-    const decoded = JSON.parse(atob(token.split('.')[1]))
-    return decoded.username
-  }
+	function getUsername() {
+		const token = window.localStorage.getItem('JWT_TOKEN');
+		if (!token) throw new Error('token is emtpy');
 
-  return {
-    newProject,
-    openProject,
-    deleteProject,
-    openFile,
-    openFolder,
-    listProjects,
-    runCode,
-    saveCode,
-    login,
-    // removeFile,
-    removeItem,
-    newFile,
-    newFolder
-  }
+		const decoded = JSON.parse(atob(token.split('.')[1]));
+		return decoded.username
+	}
+
+	return {
+		newProject,
+		openProject,
+		deleteProject,
+		openFile,
+		openFolder,
+		listProjects,
+		runCode,
+		saveCode,
+		login,
+		// removeFile,
+		removeItem,
+		newFile,
+		newFolder
+	}
 
 }
