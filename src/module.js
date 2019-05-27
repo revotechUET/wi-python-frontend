3e034f0f8855b807edd47512d401b9e8f93f0675
BuiHoang <bdhoang.ht@gmail.com>
2019-05-27 23:10:18 +0700

    update

--- src/module.js
+++ src/module.js
@@ -61,7 +61,7 @@ export default function assignAppModule(angular, moduleName) {
 			logStreamHTTP: 'http://python.dev.i2g.cloud',
 			// ONLINE_EDITOR_URL: window.__WI_PAAS_URL__ || 'https://python.i2g.cloud' || 'http://localhost:3000',
 			ONLINE_EDITOR_URL: 'http://python.dev.i2g.cloud',
-			USER_RELATED_ROOT_URL: 'https://users.i2g.cloud/login',
-			PROJECT_RELATED_ROOT_URL: 'https://api-1.i2g.cloud'
+			USER_RELATED_ROOT_URL: 'http://admin.dev.i2g.cloud/login',
+			PROJECT_RELATED_ROOT_URL: 'http://dev.i2g.cloud'
 		})
 }
