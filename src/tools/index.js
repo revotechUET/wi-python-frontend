d73ef8de20b15ac102cefe465d640642fdcbe585
Hưng Nguyễn <hungnk1996@gmail.com>
2020-08-22 11:55:23 +0700

    update

--- src/tools/index.js
+++ src/tools/index.js
@@ -11,20 +11,15 @@ import { wiLogin } from '@revotechuet/misc-component-vue'
 
 const name = 'tools'
 
-controller.$inject = ['$scope', '$timeout', 'auth', '$location', 'config', 'ngDialog']
-function controller($scope, $timeout, auth, $location, config, ngDialog) {
+controller.$inject = ['$scope', '$timeout', 'config', 'ngDialog']
+function controller($scope, $timeout, config, ngDialog) {
   const self = this
 	const BASE_URL = "https://users.i2g.cloud";
   self.$onInit = function() {
-    // self.baseUrl = $location.search().baseUrl || self.baseUrl || config.PROJECT_RELATED_ROOT_URL || BASE_URL;
-		// self.loginUrl = $location.search().loginUrl || self.loginUrl || config.USER_RELATED_ROOT_URL;
-    // self.queryString = queryString.parse(location.search);
+    loadTheme();
+    loadFontSize();
     wiLogin.doLogin({ redirectUrl: window.location.origin, whoami: config.WHOAMI, loginPage: config.AUTHENTICATION_HOME });
   }
-  // self.logout = function () {
-  //   auth.logout()
-  // }
-
   self.setShowOP = function(val) {
     $timeout(() => {$scope.showOP = val;});
   }
@@ -45,6 +40,128 @@ function controller($scope, $timeout, auth, $location, config, ngDialog) {
   self.getUser = function() {
     return localStorage.username || 'Guest';
   }
+  self.setTheme = function(theme)  {
+    if (theme == 'dark') {
+      localStorage.setItem('theme', theme);
+      $timeout(()=>{
+        self.currentTheme = 'dark';
+      })
+      $(':root').css('--toolbar-bg-color', '#252525');
+      $(':root').css('--white', '#252525');
+      $(':root').css('--black', '#fff');
+      $(':root').css('--text-primary-color', '#fff');
+      $(':root').css('--button-bg-color', '#434343');
+      $(':root').css('--input-bg', '#2d2d2d');
+      $(':root').css('--dialog-bg-color', '#333333');
+      $(':root').css('--input-bg-hover', '#1e1e1e');
+      $(':root').css('--box-shadow', '0px 10px 50px rgb(0 0 0 / 17%)');
+      $(':root').css('--body-bg-color', '#252525');
+      $(':root').css('--input-check-bg', '#333333');
+      $(':root').css('--input-border', '#fff');
+      $(':root').css('--select-bg', '#f3f3f4');
+      $(':root').css('--select-border', '#434343');
+      
+    }
+    if (theme == 'light') {
+      localStorage.setItem('theme', 'light');
+      $timeout(()=>{
+        self.currentTheme = 'light';
+      })
+      $(':root').css('--toolbar-bg-color', '#fff');
+      $(':root').css('--body-bg-color', '#f3f3f4');
+      $(':root').css('--dialog-bg-color', '#fff');
+      $(':root').css('--white', '#fff');
+      $(':root').css('--black', '#000');
+      $(':root').css('--button-bg-color', '#f3f3f4');
+      $(':root').css('--text-primary-color', '#0d0b22');
+      $(':root').css('--input-check-bg', '#fff');
+      $(':root').css('--input-bg', '#f3f3f4');
+      $(':root').css('--input-border', '#9e9ea7');
+      $(':root').css('--input-bg-hover', '#fff');
+      $(':root').css('--box-shadow', '0px 10px 50px rgba(0, 0, 0, 0.1)');
+      $(':root').css('--select-bg', '#f3f3f4');
+      $(':root').css('--select-border', '#f3f3f4');
+
+    }
+  }
+  function loadTheme() {
+    if (localStorage.getItem('theme') == '') {
+      self.setTheme('light');
+      $timeout(()=>{
+        self.currentTheme = 'light';
+      })
+    } else {
+      self.setTheme(localStorage.getItem('theme'));
+    }
+  }
+  self.setFontSize = function(size) {
+    if (size == '12') {
+      localStorage.setItem('font-size', size);
+      self.currentFont = '12';
+      $('*').addClass('font-12');
+      $('*').removeClass('font-14');
+      $('*').removeClass('font-16');
+      $('*').removeClass('font-18');
+      $('*').removeClass('font-20');
+    }
+    if (size == '14') {
+      localStorage.setItem('font-size', size);
+      self.currentFont = '14';
+      $('*').addClass('font-14');
+      $('*').removeClass('font-12');
+      $('*').removeClass('font-16');
+      $('*').removeClass('font-18');
+      $('*').removeClass('font-20');
+    }
+    if (size == '16') {
+      localStorage.setItem('font-size', size);
+      self.currentFont = '16';
+      $('*').addClass('font-16');
+      $('*').removeClass('font-14');
+      $('*').removeClass('font-12');
+      $('*').removeClass('font-18');
+      $('*').removeClass('font-20');
+    }
+    if (size == '18') {
+      localStorage.setItem('font-size', size);
+      self.currentFont = '18';
+      $('*').addClass('font-18');
+      $('*').removeClass('font-14');
+      $('*').removeClass('font-16');
+      $('*').removeClass('font-12');
+      $('*').removeClass('font-20');
+    }
+    if (size == '20') {
+      localStorage.setItem('font-size', size);
+      self.currentFont = '20';
+      $('*').addClass('font-20');
+      $('*').removeClass('font-14');
+      $('*').removeClass('font-16');
+      $('*').removeClass('font-18');
+      $('*').removeClass('font-12');
+    }
+    
+  }
+  function loadFontSize() {
+    if (localStorage.getItem('font-size') == '') {
+      self.setFontSize('14');
+      $timeout(()=>{
+        self.currentFont = '14';
+      })
+    } 
+    else {
+      self.setFontSize(localStorage.getItem('font-size'));
+    }
+  }
+  self.openSetting = function() {
+    let settingDialog = ngDialog.open({
+      template: 'settingDialog',
+      className: 'i2g-ngdialog',
+      showClose: true,
+      scope: $scope,
+    });
+  }
+  
 }
 
 export default {
@@ -78,7 +195,6 @@ export default {
       generateLoginByAccount:'<',
       generateSaveCurveData:'<',
       aboutWiPython: '<',
-      changeTheme: '<',
       autoSave: '=',
       saveWarning: '=',
       toastHistory: '=',
